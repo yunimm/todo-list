df4e9159e97ea3dd9ce177ab346444a00eb41be8
yumimm <yuu9916@gmail.com>
2021-11-26 22:17:01 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
+
+mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.on('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting the route and corresponding response
 app.get('/', (req, res) => {
